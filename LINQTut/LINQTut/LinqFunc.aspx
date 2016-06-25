315e477380da86c68bd2ef001e7aca7dbbc1f6b0
Siddharth Reddy Lucky <siddharth.luckyreddy@gmail.com>
2016-06-25 13:36:39 -0500

    Add project files.

---
+++ LINQTut/LINQTut/LinqFunc.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LinqFunc.aspx.cs" Inherits="LINQTut.Projection" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="ProjectionView" runat="server"></asp:GridView>
+        <asp:Button ID="GetDataBut" runat="server" Text="GetData" OnClick="GetDataBut_Click" />
+        <asp:Button ID="InsertBut" runat="server" OnClick="InsertBut_Click" Text="Insert" />
+        <asp:Button ID="UpdBut" runat="server" Text="Update" OnClick="UpdBut_Click" />
+        <asp:Button ID="DelBut" runat="server" Text="Delete" OnClick="DelBut_Click" />
+        <br /><br />
+        <asp:Label ID="Label1" runat="server" Text="Name"></asp:Label><asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br /><br />
+        <asp:Label ID="Label2" runat="server" Text="LName"></asp:Label><asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br /><br />
+        <asp:Button ID="InsSub" runat="server" Text="Submit" OnClick="InsSub_Click" /><br />
+        <asp:Label ID="UpdOldlab" runat="server" Text="Old LName"></asp:Label><asp:TextBox ID="UpdOldLName" runat="server"></asp:TextBox><br /><br />
+        <asp:Label ID="UpdNewlab" runat="server" Text="New LName"></asp:Label><asp:TextBox ID="UpdNewLName" runat="server"></asp:TextBox><br /><br />
+        <asp:Button ID="UpdSubmit" runat="server" Text="Submit" OnClick="UpdSubmit_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ LINQTut/LINQTut/LinqFunc.aspx.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LINQTut
+{
+    public partial class Projection : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Insert Label and Textboxes
+            Label1.Visible = false;
+            Label2.Visible = false;
+            TextBox1.Visible = false;
+            TextBox2.Visible = false;
+            InsSub.Visible = false;
+            //Update labels and textBoxes
+            UpdNewLName.Visible = false;
+            UpdOldlab.Visible = false;
+            UpdOldLName.Visible = false;
+            UpdSubmit.Visible = false;
+            UpdNewlab.Visible = false;
+
+            //Code for projecting required columns
+
+            //LinqClassDataContext context = new LinqClassDataContext();
+            //{
+            //    var result = from value in context.ProTabs
+            //                 select new { value.LName };
+            //    ProjectionView.DataSource = result;
+            //    ProjectionView.DataBind();
+            //}
+        }
+
+        protected void GetDataBut_Click(object sender, EventArgs e)
+        {
+            //Labels and textboxes of Insert
+            Label1.Visible = false;
+            Label2.Visible = false;
+            TextBox1.Visible = false;
+            TextBox2.Visible = false;
+            InsSub.Visible = false;
+            //Labels and textboxes of Update
+            UpdNewLName.Visible = false;
+            UpdOldlab.Visible = false;
+            UpdOldLName.Visible = false;
+            UpdSubmit.Visible = false;
+            UpdNewlab.Visible = false;
+            GetData();
+        }        
+
+        protected void InsertBut_Click(object sender, EventArgs e)
+        {
+            //Labels and textboxes of Insert
+            Label1.Visible = true;
+            Label2.Visible = true;
+            TextBox1.Visible = true;
+            TextBox2.Visible = true;
+            InsSub.Visible = true;
+            //Labels and textboxes of Update
+            UpdNewLName.Visible = false;
+            UpdOldlab.Visible = false;
+            UpdOldLName.Visible = false;
+            UpdSubmit.Visible = false;
+            UpdNewlab.Visible = false;
+
+        }
+
+        protected void InsSub_Click(object sender, EventArgs e)
+        {
+            InsertData();
+        }
+
+        protected void UpdBut_Click(object sender, EventArgs e)
+        {
+            UpdNewLName.Visible = true;
+            UpdOldlab.Visible = true;
+            UpdOldLName.Visible = true;
+            UpdSubmit.Visible = true;
+            UpdNewlab.Visible = true;
+        }
+
+        protected void UpdSubmit_Click(object sender, EventArgs e)
+        {
+            UpdateMethod();
+        }
+        //Method implemented on direct button click
+        protected void DelBut_Click(object sender, EventArgs e)
+        {
+            using (LinqClassDataContext delete = new LinqClassDataContext())
+            {
+                ProTab1 delvalue = delete.ProTab1s.SingleOrDefault(value => value.LName=="lorry");
+                delete.ProTab1s.DeleteOnSubmit(delvalue);
+                delete.SubmitChanges();
+                GetData();
+            }
+        }
+
+
+    //Method storage area
+        //Method to update the table
+        private void UpdateMethod()
+        {
+            using (LinqClassDataContext update = new LinqClassDataContext())
+            {
+                ProTab1 insMet = update.ProTab1s.SingleOrDefault(value => value.LName == UpdOldLName.Text);
+                insMet.LName = UpdNewLName.Text.ToString();
+                update.SubmitChanges();
+                GetData();
+            }
+        }
+
+        //Method contains Code for insertion
+        private void InsertData()
+        {
+            using (LinqClassDataContext context = new LinqClassDataContext())
+            {
+                ProTab1 instab = new ProTab1
+                {
+                    Name = TextBox1.Text.ToString(),
+                    LName = TextBox2.Text.ToString()
+                };
+                context.ProTab1s.InsertOnSubmit(instab);
+                context.SubmitChanges();
+                GetData();
+            }
+        }
+
+        //Method to DisplayData
+        private void GetData()
+        {
+            LinqClassDataContext context = new LinqClassDataContext();
+            {
+                //Logger is used to log data and provide query syntax
+                context.Log = Response.Output;
+                ProjectionView.DataSource = context.ProTab1s;
+                ProjectionView.DataBind();
+            }
+        }
+
+    }
+}
